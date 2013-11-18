81aaa94e4662f8420e1286e6e04861ef75091efe
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2013-11-18 20:12:29 +0900

    Add some test

--- test/redis.rb
+++ test/redis.rb
@@ -5,13 +5,18 @@
 assert("Redis#set, Redis#get") do
   r = Redis.new "127.0.0.1", 6379
   r.set "hoge", "fuga"
-  r.get("hoge") == "fuga"
+  assert_equal("fuga", r.get("hoge"))
 end
 
 assert("Redis#[]=, Redis#[]") do
   r = Redis.new "127.0.0.1", 6379
   r["hoge"] = "fuga"
-  r["hoge"] == "fuga"
+  assert_equal("fuga", r["hoge"])
+end
+
+assert("Redis#select") do
+  r = Redis.new "127.0.0.1", 6379
+  assert_equal(r, r.select(0))
 end
 
 assert("Redis#exist?") do
@@ -20,3 +25,13 @@ assert("Redis#exist?") do
   assert_true(r.exists?("hoge"))
   assert_false(r.exists?("fuga"))
 end
+
+# TODO: Add test
+# - randomkey
+# - del
+# - incr
+# - decr
+# - lpush
+# - lrange
+# - ltrim
+# - publish
