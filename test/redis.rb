f7c6fe3ed2aa5a4e1e8502f45db5d91ddc6c2481
Jun Hiroe <Jun.Hiroe@gmail.com>
2013-12-16 23:36:34 +0900

    Add Redis#hdel test

--- test/redis.rb
+++ test/redis.rb
@@ -54,6 +54,24 @@ assert("Redis#hset", "Redis#hget") do
   assert_equal "c", ret_get_f2
 end
 
+assert("Redis#hdel") do
+  r = Redis.new "127.0.0.1", 6379
+  r.del "myhash"
+
+  r.hset "myhash", "field1", "a"
+  r.hset "myhash", "field2", "b"
+  ret_get_f1_a = r.hget "myhash", "field1"
+  r.hdel "myhash", "field1"
+  ret_get_f1_nil = r.hget "myhash", "field1"
+  ret_exists = r.exists?("myhash")
+
+  r.close
+
+  assert_equal "a", ret_get_f1_a
+  assert_nil ret_get_f1_nil
+  assert_true ret_exists
+end
+
 # got erro for travis ci. comment out until fix the problems
 #assert("Redis#zadd, Redis#zrange") do
 #  r = Redis.new "127.0.0.1", 6379
